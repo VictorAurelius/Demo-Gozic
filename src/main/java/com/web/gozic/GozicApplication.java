fa12cfbedfa85dd02d3a9b8bbd2f1c83f77af498
VictorAurelius <vankiet14491@gmail.com>
2025-05-15 16:53:29 +0700

    init

---
+++ src/main/java/com/web/gozic/GozicApplication.java
@@ -0,0 +1,13 @@
+package com.web.gozic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GozicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GozicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/web/gozic/GozicApplicationTests.java
@@ -0,0 +1,13 @@
+package java.web.gozic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GozicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
